fe27b8d70c991288f7c5fab58cb461310391e39c
Radamesjrr <radames.dev01@gmail.com>
2025-04-28 22:48:55 -0300

    Projeto Mario Broos

---
+++ script.js
@@ -0,0 +1,29 @@
+const formulario = document.querySelector (".form-orçamento")
+const mascara = document.querySelector (".mascara-formulario")
+const servicos = document.querySelector(".div-servicos")
+const mascara2 = document.querySelector(".mascara-servicos")
+
+function mostrarformulario() {
+    formulario.style.left ="650px"
+    mascara.style.visibility = "visible"
+}
+
+function esconderformulario() {
+    mascara.style.visibility="hidden"
+    formulario.style.left ="-230px"
+}
+
+function mostrarserviços() {
+    servicos.style.right ="590px"
+    mascara2.style.visibility ="visible"
+}
+
+function esconderservicos() {
+    mascara2.style.visibility="hidden"
+    servicos.style.right= "-290px"
+}
+
+
+
+
+
